645736c28a9b1c891a98238b5b20ad89d3449a6f
azraeljack <4312182lyp@gmail.com>
2018-06-27 16:25:32 -0400

    Format logging

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -3,6 +3,7 @@ import * as pino from 'pino';
 const l: pino.Logger = pino({
     name: process.env.APP_ID,
     level: !!process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'debug',
+    prettyPrint: true
 });
 
 export { l };
--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -6,7 +6,7 @@ const logger: any = (req: Request, res: Response, next: NextFunction) => {
     const requestedURL: string = req.url;
     res.on('finish', () => {
         const duration: number = Date.now() - start;
-        l.info(`[${req.method}] [${requestedURL}] [${res.statusCode}] [${req.ip}] [${new Date().toISOString()}] [${duration}ms]`);
+        l.info(`[${req.method}] [${requestedURL}] [${res.statusCode}] [${req.ip}] [${duration}ms]`);
     });
     next(); // Passing the request to the next handler in the stack.
 };
